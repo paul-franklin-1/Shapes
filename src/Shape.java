722e3857b225bf4bc0323e039f7bcba5cb850205
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-13 20:23:11 -0400

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,5 @@
+abstract class Shape {
+    abstract double surface_area();
+    abstract double volume();
+
+}
---
+++ src/ShapeArray.java
@@ -0,0 +1,5 @@
+public class ShapeArray {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
