c058b39b8bbd8922fc6280018e66fdbea4a9be2f
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-13 22:04:55 -0400

    closing in on completion

--- src/Cone.java
+++ src/Cone.java
@@ -1,11 +1,23 @@
 public class Cone extends Shape{
-    @Override
-    double surface_area() {
-        return 0;
+    private double radius;
+    private double height;
+    public Cone (double radius, double height){
+        this.radius = radius;
+        this.height = height;}
+
+    double surface_area(){
+        return Math.PI * radius * (radius + Math.sqrt(height * height + radius * radius));
     }
 
     @Override
     double volume() {
-        return 0;
+        return (1.0 / 3.0) * Math.PI * radius * radius * height;
+    }
+
+    public String toString(){
+        String str = "";
+        str = "Cone surface area: " +  surface_area() +
+                "\nCone volume: " + volume();
+        return str;
     }
 }
